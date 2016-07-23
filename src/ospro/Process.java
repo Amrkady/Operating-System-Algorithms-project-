cf8789a663d3b3c28df823f4a037a8ae8182cafb
Alkady <Alkady@Alkady-PC>
2016-07-23 17:58:52 +0200

    Operating System Algorithms  project

---
+++ src/ospro/Process.java
@@ -0,0 +1,23 @@
+
+package ospro;
+
+
+public class Process {
+   private int burstTime, arrivalTime;
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+}
